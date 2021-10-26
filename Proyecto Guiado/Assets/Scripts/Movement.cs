53fdce7d46793496b5321775b6d67200c7828360
Ana <anagigv1@gmail.com>
2021-10-26 14:59:17 -0500

    Proyecto base

---
+++ Proyecto Guiado/Assets/Scripts/Movement.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Maria
+{
+    public class Movement : MonoBehaviour
+    {
+        [SerializeField] float linearspeed = 8f, rotSpeed = 90;
+        [SerializeField] string horizontal = "Horizontal", vertical = "Vertical", dash = "dash";
+        [SerializeField] float dashForce = 1500;
+
+        Rigidbody rbody;
+        AudioSource motor;
+        Light lightL;
+        Light lightR;
+        void Start()
+        {
+            motor = GetComponent<AudioSource>();
+            lightL = transform.Find("LeftLight").GetComponent<Light>();
+            lightR = transform.Find("RightLight").GetComponent<Light>();
+            rbody = GetComponent<Rigidbody>();
+        }
+        void Update()
+        {
+            Dash();
+        }
+        public void Move()
+        {
+            Vector3 dirZ = transform.forward;
+            Vector3 velocityZ = linearspeed * dirZ * Input.GetAxis(vertical);
+            transform.position += velocityZ * Time.deltaTime;
+        }
+        public void Rotate()
+        {
+            Vector3 dirRot = new Vector3(0, 1, 0);
+            Vector3 velocityRot = rotSpeed * dirRot * Input.GetAxis(horizontal);
+            transform.eulerAngles += velocityRot * Time.deltaTime;
+        }
+        public void Sound()
+        {
+            if (Input.GetAxis(horizontal) != 0 || Input.GetAxis(vertical) != 0) {
+                motor.volume = 0.2f;
+            }
+            else {
+                motor.volume = 0.03f;
+            }
+        }
+        public void SetLights()
+        {
+            if (Input.GetAxis(vertical) < 0) {
+                lightL.intensity = 2;
+                lightR.intensity = 2;
+            }
+            else {
+                if (Input.GetAxis(horizontal) < 0) {
+                    lightL.intensity = 2;
+                    lightR.intensity = 0;
+                }
+                else if (Input.GetAxis(horizontal) > 0)
+                {
+                    lightL.intensity = 0;
+                    lightR.intensity = 2;
+                }
+                else if (Input.GetAxis(horizontal) == 0)
+                {
+                    lightL.intensity = 0;
+                    lightR.intensity = 0;
+                }
+            }
+        }
+        void Dash()
+        {
+            if (Input.GetButtonDown(dash))
+            {
+                Vector3 dir = (transform.forward * 5 + transform.up * 1).normalized;
+                Vector3 force = dashForce * dir * 1;
+                rbody.AddForce(force);
+            }
+        }
+    }
+}
---
+++ Proyecto Guiado/Assets/Scripts/Projectile.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    
+    [SerializeField] float force = 10, lifetime = 0;
+    void Update()
+    {
+        lifetime += Time.deltaTime;
+        if (lifetime >= 5) Destroy(gameObject);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameObject target = collision.gameObject;
+        if(target.CompareTag("Player"))
+        {
+            PlayerController player = target.GetComponent<PlayerController>();
+            player.Hit(force);
+            Destroy(gameObject); //Destruir el proyector para que no haga daño 2 veces
+
+        }
+    }
+}
